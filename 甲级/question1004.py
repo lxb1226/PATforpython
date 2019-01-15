cad4d898c9c9ac218cdd7048ecf02592b1443cb4
lxb1226 <1944303766@qq.com>
2019-01-15 15:56:44 +0800

    有一个测试点没过

--- 甲级/question1004.py
+++ 甲级/question1004.py
@@ -1,1 +1,26 @@
+def dfs(node, res, index, depth):
+    global maxDepth
+    if len(node[index]) == 0:
+        res[depth] += 1
+        if maxDepth < depth:
+            maxDepth = depth
+        return
+    for i in range(len(node[index])):
+        dfs(node, res, node[index][i], depth + 1)
 
+
+M, N = [int(x) for x in input().split(" ")]
+node = [[] for _ in range(100)]
+res = [0 for _ in range(100)]
+maxDepth = 0
+for i in range(N):
+    temp = [int(x) for x in input().split(' ')]
+    ID = temp[0]
+    K = temp[1]
+    node[int(ID)] = temp[2:]
+# print(node)
+
+dfs(node, res, 1, 0)
+for i in range(maxDepth):
+    print(res[i], end=" ")
+print(res[maxDepth], end="")
